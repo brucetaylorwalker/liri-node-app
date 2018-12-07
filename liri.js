e2c8921d3d12b308a324b0e22fda2a58bffed1b6
Taylor Walker <taylor.walker@hotmail.com>
2018-12-07 06:11:12 -0800

    pseudo code/ start liri code/ imp spotify npm

--- liri.js
+++ liri.js
@@ -1,12 +1,38 @@
-var spotify = new spotify(keys.spotify);
+require("dotenv").config();
 
+//import keys.js
+var fs = require("fs");
+var keys = require("./keys");
+
+//arguments
 
 //commands//
-//`concert-this`
-// `spotify-this-song`
-// `movie-this`
-// `do-what-it-says`
+//`concert-this <artist/band name here>`***SECOND***
+    //venue name
+    //venue location
+    //date of event (moment "MM/DD/YYYY")
 
-//spotify
-//Client ID 0908decda91943c7b5414ad741a896b3
-//Client Secret ddaf0d2a0a91439ba504c4c3a8b06324
+    var spotify = new spotify(keys.spotify);
+//node-spotify-api
+// `spotify-this-song`***THIRD***
+    //artist(s)
+    //song name
+    //preview link of song from spotify
+    //album song is from
+    //default
+
+// `movie-this`***FIRST***
+    // Title of the movie.
+    // Year the movie came out.
+    // IMDB Rating of the movie.
+    // Rotten Tomatoes Rating of the movie.
+    // Country where the movie was produced.
+    // Language of the movie.
+    // Plot of the movie.
+    // Actors in the movie.
+    //default
+
+// `do-what-it-says`***FOURTH***
+    //run spotify-this-song from random.txt
+
+
