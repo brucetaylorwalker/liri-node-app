d51dd0e2c77d8c272b0660a77e7641756259a84a
Taylor Walker <taylor.walker@hotmail.com>
2018-12-06 16:26:08 -0800

    files created

---
+++ liri.js
@@ -0,0 +1,12 @@
+var spotify = new spotify(keys.spotify);
+
+
+//commands//
+//`concert-this`
+// `spotify-this-song`
+// `movie-this`
+// `do-what-it-says`
+
+//spotify
+//Client ID 0908decda91943c7b5414ad741a896b3
+//Client Secret ddaf0d2a0a91439ba504c4c3a8b06324
